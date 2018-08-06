7c25175f8f012934f774f06c077f8f44d7e7fa24
Javier <16022934@myrp.edu.sg>
2018-08-06 11:00:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022934/demonavigation/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a16022934.demonavigation;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+            }
+        });
+
+
+    }
+}
